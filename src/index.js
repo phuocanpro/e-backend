8677a5f3b278f3b6e1bb98d65e44e16ff4d0c344
phuocanpro <anhtp.22ite@vku.udn.vn>
2023-10-09 02:30:10 +0700

    up code 17

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3002;
-app.get("/", (req, res) => {
-  res.send("Hello World! everyone");
-});
 
-console.log('process.env.MONGO_DB',process.env.MONGO_DB)
+app.use(bodyParser.json());
 
-mongoose.connect(`mongodb+srv://phuocanpro:${process.env.MONGO_DB}@phuocanpro.v5lwdg9.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => {
-    console.log('Connect success!')
-})
-.catch((err) =>{
-    console.log(err)
-})
+routes(app);
+
+mongoose.connect(`${process.env.MONGO_DB}`)
+  .then(() => {
+      console.log('Connect success!')
+  })
+  .catch((err) =>{
+      console.log(err)
+  })
+
+
 
 app.listen(port, () => {
   console.log("server is running in port: ", +port);
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const UserRouter = require('./UserRouter')
+
+const routes = (app) => {
+    app.use('/api/user', UserRouter)
+  
+}
+
+module.exports = routes
